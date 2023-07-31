f09f11e4180d2d38c47f463e4fb520123c50868f
Murilo Klein <91572912+MUR1L0K@users.noreply.github.com>
2023-07-31 17:34:19 -0300

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,18 @@
 const apiKey = "1407d58240fb0125b4512f467a9b0a50";
 const apiCountryUrl = "https://flagsapi.com/png";
-
 const cityInput = document.querySelector("#cityInput");
 const secondPart = document.querySelector("#mainContentSecondPart");
 const secondPartButtons = document.querySelectorAll(
   ".mainContentButtonsPartButtons"
 );
-const carregando = document.querySelector("#carregando");
+const loadingPart = document.querySelector("#loadingPart");
 const erro = document.querySelector("#erro");
 const weatherData = document.querySelector("#weatherData");
 const cityElement = document.querySelector("#city");
 const wind = document.querySelector("#wind span");
 const description = document.querySelector("#description");
 const weatherIcon = document.querySelector("#weatherIcon");
-const umidity = document.querySelector("#umidity span");
+const humidity = document.querySelector("#humidity span");
 const temperature = document.querySelector("#temperature span");
 const countryFlag = document.querySelector("#coutnryFlag");
 
@@ -25,31 +24,42 @@ const getWeatherData = async (city) => {
 };
 
 const loading = async () => {
-  carregando.classList.remove("hide");
+  loadingPart.classList.remove("hide");
   secondPart.classList.add("hide");
   weatherData.classList.add("hide");
   erro.classList.add("hide");
-  setTimeout(() => {
-    carregando.classList.add("hide");
-  }, 2000);
+};
+
+const hideLoading = async (error) => {
+  loadingPart.classList.add("hide");
+  if (error == 1) {
+    erro.classList.remove("hide");
+  } else {
+    weatherData.classList.remove("hide");
+  }
 };
 
 const showWeather = async (city) => {
   await loading();
   const data = await getWeatherData(city);
-  if (data.message == "city not found") {
-    erro.classList.remove("hide");
+  if (data.message == "city not found" || city == "") {
+    hideLoading(1);
+    cityInput.value = "";
   } else {
-    weatherData.classList.remove("hide");
+    countryFlag.src = await getFlag(data.sys.country);
     cityElement.innerText = data.name;
     temperature.innerText = parseInt(data.main.temp);
-    umidity.innerText = data.main.humidity;
+    humidity.innerText = data.main.humidity;
     wind.innerText = data.wind.speed.toFixed(1);
     description.innerText = data.weather[0].description;
     weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    countryFlag.src = `https://flagsapi.com/${data.sys.country}/shiny/64.png`;
-    console.log(data);
+    cityInput.value = "";
+    hideLoading(0);
   }
+};
+
+const getFlag = async (country) => {
+  return `https://flagsapi.com/${country}/shiny/64.png`;
 };
 
 document.querySelector("#searchButton").addEventListener("click", (ev) => {
@@ -57,12 +67,12 @@ document.querySelector("#searchButton").addEventListener("click", (ev) => {
   showWeather(cityInput.value);
 });
 
-//secondPartButtons.forEach(
-//  addEventListener("click", (ev) => {
-//    ev.preventDefault();
-//    showWeather(ev.target.value);
-//  })
-//);
+secondPartButtons.forEach((input) => {
+  input.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    showWeather(ev.target.value);
+  });
+});
 
 cityInput.addEventListener("keyup", (ev) => {
   if (ev.code == "Enter") {
